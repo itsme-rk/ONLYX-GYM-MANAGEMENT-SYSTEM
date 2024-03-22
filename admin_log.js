4d1644864ee158249e05f5f7be7fe67ae5404ff9
itsme-rk <karthikramesh1813@gmail.com>
2024-03-23 01:43:49 +0530

    !small changes

--- admin_log.js
+++ admin_log.js
@@ -2,12 +2,12 @@ function checkCredentials() {
     var adminUsername = document.getElementById("adminUsername").value;
     var adminPassword = document.getElementById("adminPassword").value;
 
-    // Check if the entered credentials match the predefined admin credentials
+    
     if (adminUsername === 'admin' && adminPassword === 'password') {
         // Redirect to admin.html if login is successful
         window.location.href = 'admin.html';
     } else {
-        // Display an alert for wrong credentials
+        
         alert('Wrong credentials. Please try again.');
     }
 }
